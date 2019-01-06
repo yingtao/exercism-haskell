35d76a91a48229c7f2e04136010de59073630746
Yingtao Jiang <yingtao.jiang@gmail.com>
2019-01-06 15:51:11 -0800

    first implementation.

--- binary-search-tree/src/BST.hs
+++ binary-search-tree/src/BST.hs
@@ -10,28 +10,38 @@ module BST
     , toList
     ) where
 
-data BST a = Dummy deriving (Eq, Show)
+data BST a = Empty | Node a (BST a) (BST a) deriving (Eq, Show)
 
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft tree = error "You need to implement this function."
+bstLeft tree = case tree of
+    Empty -> Nothing
+    Node a left right -> Just left
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight tree = error "You need to implement this function."
+bstRight tree = case tree of 
+    Empty -> Nothing
+    Node a left right -> Just right
 
 bstValue :: BST a -> Maybe a
-bstValue tree = error "You need to implement this function."
+bstValue tree = case tree of
+    Empty -> Nothing
+    Node a left right -> Just a
 
 empty :: BST a
-empty = error "You need to implement this function."
+empty = Empty
 
 fromList :: Ord a => [a] -> BST a
-fromList xs = error "You need to implement this function."
+fromList xs = foldr (insert) Empty (reverse xs)
 
 insert :: Ord a => a -> BST a -> BST a
-insert x tree = error "You need to implement this function."
+insert x tree = case tree of
+    Empty -> singleton x
+    Node a left right -> if x <= a then Node a (insert x left) right else Node a left (insert x right)
 
 singleton :: a -> BST a
-singleton x = error "You need to implement this function."
+singleton x = Node x Empty Empty
 
 toList :: BST a -> [a]
-toList tree = error "You need to implement this function."
+toList tree = case tree of
+    Empty -> []
+    Node a left right -> toList left ++ [a] ++ toList right
