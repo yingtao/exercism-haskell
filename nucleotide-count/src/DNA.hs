077efb0ca887f58042e13cfef9737c78ade7cac7
Yingtao Jiang <yingtao.jiang@gmail.com>
2019-01-06 00:14:39 -0800

    following aammff's suggestion to auto derive Enum, Bounded, Read and use readMaybeEither to simplify the code

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,23 +1,29 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map)
+import Text.Read
 
 import qualified Data.Map.Strict as SM
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Bounded, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts [] = Right $ SM.fromList [(A,0),(C,0),(G,0),(T,0)] 
 nucleotideCounts (x:xs) = 
-    case fromChar(x) of
+    case readEither [x] of
         Right y -> case (nucleotideCounts xs) of
             Right m -> Right $ SM.adjust (+1) y m
             Left e' -> Left e'
         Left e  -> Left e
 
-fromChar :: Char -> Either String Nucleotide
-fromChar 'A' = Right A
-fromChar 'C' = Right C
-fromChar 'G' = Right G
-fromChar 'T' = Right T
-fromChar  c  = Left ("Invalid Symbol " ++ show c)
+-- f :: Either String (Map Nucleotide Int) -> Char -> Either String (Map Nucleotide Int)
+-- f (Left x) _ = Left x
+-- f (Right ) 
+
+
+-- fromChar :: Char -> Either String Nucleotide
+-- fromChar 'A' = Right A
+-- fromChar 'C' = Right C
+-- fromChar 'G' = Right G
+-- fromChar 'T' = Right T
+-- fromChar  c  = Left ("Invalid Symbol " ++ show c)
