cc9ba0ef4e6253ae6e74795e21cff9ec6f3af2c8
Yingtao Jiang <yingtao.jiang@gmail.com>
2019-01-06 01:27:21 -0800

    change to foldl first to simplify the code.

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -2,28 +2,18 @@ module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map)
 import Text.Read
+import Control.Monad (foldM)
 
 import qualified Data.Map.Strict as SM
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Bounded, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts [] = Right $ SM.fromList [(A,0),(C,0),(G,0),(T,0)] 
-nucleotideCounts (x:xs) = 
-    case readEither [x] of
-        Right y -> case (nucleotideCounts xs) of
-            Right m -> Right $ SM.adjust (+1) y m
-            Left e' -> Left e'
-        Left e  -> Left e
+nucleotideCounts xs = foldl merge (Right $ SM.fromList [(x,0)| x <- [minBound .. maxBound]]) xs
 
--- f :: Either String (Map Nucleotide Int) -> Char -> Either String (Map Nucleotide Int)
--- f (Left x) _ = Left x
--- f (Right ) 
-
-
--- fromChar :: Char -> Either String Nucleotide
--- fromChar 'A' = Right A
--- fromChar 'C' = Right C
--- fromChar 'G' = Right G
--- fromChar 'T' = Right T
--- fromChar  c  = Left ("Invalid Symbol " ++ show c)
+merge :: Either String (Map Nucleotide Int) -> Char -> Either String (Map Nucleotide Int)
+merge cur char = case readEither [char] of
+    Left e -> Left e
+    Right a -> case cur of
+        Right m -> Right $ SM.adjust (+1) a m
+        Left e' -> Left e'
