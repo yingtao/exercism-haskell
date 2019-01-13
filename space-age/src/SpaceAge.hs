d559fc6c4a765a79189b397a704b0bd6e3cacf60
Yingtao Jiang <yingtao.jiang@gmail.com>
2019-01-12 23:29:35 -0800

    reduce magic number to name the const number

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -11,11 +11,13 @@ data Planet = Mercury
 
 ageOn :: Planet -> Float -> Float
 ageOn planet seconds = case planet of
-    Mercury -> seconds / (31557600 * 0.2408467)
-    Venus   -> seconds / (31557600 * 0.61519726)
-    Earth   -> seconds / 31557600
-    Mars    -> seconds / (31557600 * 1.8808158)
-    Jupiter -> seconds / (31557600 * 11.862615)
-    Saturn  -> seconds / (31557600 * 29.447498)
-    Uranus  -> seconds / (31557600 * 84.016846)
-    Neptune -> seconds / (31557600 * 164.79132)
+    Mercury -> seconds / (earthyearseconds * 0.2408467)
+    Venus   -> seconds / (earthyearseconds * 0.61519726)
+    Earth   -> seconds / earthyearseconds
+    Mars    -> seconds / (earthyearseconds * 1.8808158)
+    Jupiter -> seconds / (earthyearseconds * 11.862615)
+    Saturn  -> seconds / (earthyearseconds * 29.447498)
+    Uranus  -> seconds / (earthyearseconds * 84.016846)
+    Neptune -> seconds / (earthyearseconds * 164.79132)
+
+    where earthyearseconds = 31557600
